ebe52c8c3efefcb5faf6294a793556db1a15acb9
veeresh-111 <68288620+veeresh-111@users.noreply.github.com>
2024-09-10 23:21:28 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-bucket-vsk-01"  # Replace with your actual S3 bucket name
+    bucket = "my-bucket-vsk-001"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
     region = "us-east-1" 
   }
