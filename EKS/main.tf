9251107438bd0baec53c5d24b091390c2ada74c7
veeresh-111 <68288620+veeresh-111@users.noreply.github.com>
2024-11-28 22:26:48 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -43,7 +43,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.public.ids
+    subnet_ids = data.aws_subnets.public.ids[0]
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
@@ -88,7 +88,7 @@ resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.example.name
   node_group_name = "Node-cloud"
   node_role_arn   = aws_iam_role.example1.arn
-  subnet_ids      = data.aws_subnets.public.ids
+  subnet_ids      = data.aws_subnets.public.ids[0]
 
   scaling_config {
     desired_size = 1
